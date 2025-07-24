c6f9c1596adb27ce0d197603c06743df4473794b
DESKTOP-5FU4EFM\Home <huhjhace@gmail.com>
2025-07-24 17:47:51 +0900

    2025.7.24

--- src/main/java/hello/hello_spring/controller/HomeController.java
+++ src/main/java/hello/hello_spring/controller/HomeController.java
@@ -1,12 +1,11 @@
 package hello.hello_spring.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/") //slash떄문에 localhost:8080으로 들어오면 이게 호출이 된다고 함
     public String home() {
         return "home";
     }
